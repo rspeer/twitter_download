c5df253c8ac50a9fc04ed5c4e797de8475e196d6
Rob Speer <rob@luminoso.com>
2015-08-05 16:03:32 -0400

    Automatically clean glitches in the SemEval 2014 data

--- download_tweets_api.py
+++ download_tweets_api.py
@@ -1,8 +1,11 @@
 import sys
 import os
+import re
 import time
 import datetime
 import argparse
+import ftfy
+from ftfy.fixes import decode_escapes
 
 from twitter import *
 
@@ -26,32 +29,62 @@ cache = {}
 if args.partial != None:
     for line in args.partial:
         fields = line.strip().split("\t")
-        text = fields[-1]
+        text = fields[3]
         sid = fields[0]
         cache[sid] = text
 
 for line in args.dist:
     fields = line.strip().split('\t')
-    sid = fields[0]
-    uid = fields[1]
+    if len(fields) == 3:
+        sid, uid, label = fields
+        text = None
+    else:
+        sid, uid, label, text = fields[:4]
 
-    while not sid in cache:
-        try:
-            text = t.statuses.show(_id=sid)['text'].replace('\n', ' ').replace('\r', ' ')
-            cache[sid] = text
-        except TwitterError as e:
-            if e.e.code == 429:
-                rate = t.application.rate_limit_status()
-                reset = rate['resources']['statuses']['/statuses/show/:id']['reset']
-                now = datetime.datetime.today()
-                future = datetime.datetime.fromtimestamp(reset)
-                seconds = (future-now).seconds+1
-                if seconds < 10000:
-                    sys.stderr.write("Rate limit exceeded, sleeping for %s seconds until %s\n" % (seconds, future))
-                    time.sleep(seconds)
-            else:
-                cache[sid] = 'Not Available'
+    if uid.startswith('T'):
+        # Only try to download things from Twitter that are actually tweets
+        while not sid in cache:
+            try:
+                text = t.statuses.show(_id=sid)['text'].replace('\n', ' ').replace('\r', ' ')
+                cache[sid] = text
+            except TwitterError as e:
+                if e.e.code == 429:
+                    rate = t.application.rate_limit_status()
+                    reset = rate['resources']['statuses']['/statuses/show/:id']['reset']
+                    now = datetime.datetime.today()
+                    future = datetime.datetime.fromtimestamp(reset)
+                    seconds = (future-now).seconds+1
+                    if seconds < 10000:
+                        sys.stderr.write("Rate limit exceeded, sleeping for %s seconds until %s\n" % (seconds, future))
+                        time.sleep(seconds)
+                else:
+                    cache[sid] = 'Not Available'
+        text = cache[sid]
+    else:
+        assert text is not None, "Non-Twitter lines must contain text"
 
-    text = cache[sid]
+        # Some non-Twitter messages were written with messy Pythonesque
+        # Unicode escapes, such as "I\u2019ll come after lunch". Fixing these
+        # correctly is subtle (the unicode-escape codec is NOT sufficient),
+        # but this function from ftfy does much of the job:
+        text = decode_escapes(text)
 
-    args.output.write("\t".join(fields + [text]) + '\n')
+        # Now we need to remove remaining unnecessary backslashes, and the
+        # weird labels in the LJ entries:
+        text = re.sub(r'\\(.)', r'\1', text).replace(
+            'LLLINKKK', ''
+        ).replace(
+            'IIIMAGEEE', ''
+        )
+
+    # Apply ftfy's automatic fixes to the text we received.
+    # Some cases where this helps:
+    #
+    # - Decoding HTML escapes that we were supposed to interpret from Twitter,
+    #   such as &amp; -> &
+    # - Fixing some of the encoding snafus in the SMS messages, such as
+    #   "when are Ã¼ going to book?" -> "when are ü going to book?"
+    text = ftfy.fix_text(text)
+
+    args.output.write("\t".join([sid, uid, label, text]) + '\n')
+
